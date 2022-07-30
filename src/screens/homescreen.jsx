02690f3d46474acbc8e39a34e302fbc880f644ff
Fiston-pro <byiringiroetienne2@gmail.com>
2022-07-30 10:41:43 +0200

    Added a screen folder and moved everything to the homescreen file

---
+++ src/screens/homescreen.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from '../components/header'
+import Nav from '../components/nav'
+import About from '../components/about'
+import Experience from '../components/experience'
+import Service from '../components/service'
+import Portofolio from '../components/portofolio'
+import Testimonial from '../components/testimonial'
+import Contact from '../components/contact'
+import Footer from '../components/footer'
+
+const Homescreen = () => {
+    return (
+        <div>
+            <Header/>
+            <Nav/>
+            <About/>
+            <Experience/>
+            <Service/>
+            <Portofolio/>
+            <Testimonial/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Homescreen;
