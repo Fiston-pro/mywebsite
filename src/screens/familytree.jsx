2ad1def1c52f01275fa39882f1f530dceb56ad01
Fiston-pro <byiringiroetienne2@gmail.com>
2022-07-30 12:08:49 +0200

    I added family tree page

---
+++ src/screens/familytree.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Familytree = () => {
+    return (
+        <div>
+            <h1>MY Family tree, you'all</h1>
+        </div>
+    );
+};
+
+export default Familytree;
