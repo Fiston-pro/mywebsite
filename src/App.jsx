77b719644ca5bc0f4823a8c9b8df7ec5e85d2d34
Fiston-pro <byiringiroetienne2@gmail.com>
2022-07-30 12:09:38 +0200

    Added in app.jsx a way to move to different pages using router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+//Pages
 import Homescreen from './screens/homescreen'
-
-
+import Familytree from './screens/familytree';
 
 const App = () => {
   return (
-    <div> 
-        <Homescreen/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homescreen />} />
+        <Route path="family-tree" element={<Familytree />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
